b5b4d2f930191ae9c84d0980ea6514d1fea24c33
Bryan <bryanhit@gmail.com>
2021-08-21 20:03:07 -0500

    add svg support, babel, tests, navigation

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,18 @@
 module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
+  plugins: [
+    [
+      require.resolve('babel-plugin-module-resolver'),
+      {
+        alias: {
+          _assets: './src/assets',
+          _components: './src/components',
+          _constants: './src/constants',
+          _services: './src/services',
+          _store: './src/store',
+          // _storybook: './storybook',
+        },
+      },
+    ],
+  ]
 };
---
+++ src/components/Navigation/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+
+const Navigation = (props) => {
+  return (
+    <NavigationContainer {...props}>
+
+    </NavigationContainer>
+  );
+}
+
+export default Navigation;
