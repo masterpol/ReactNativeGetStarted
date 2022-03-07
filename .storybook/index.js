eaa44abcebc9faefc5f1ac61b59fead6e958dceb
Bryan <bryanhit@gmail.com>
2022-03-07 18:35:28 -0500

    update base

---
+++ .storybook/index.js
@@ -0,0 +1,7 @@
+import 'react-native-gesture-handler'
+import { getStorybookUI } from '@storybook/react-native';
+import './storybook.requires';
+
+const StorybookUIRoot = getStorybookUI({});
+
+export default StorybookUIRoot;
